201e66b84701caa6a6b650f498f11bd213f9afc8
JMiller686 <jmiller686@gmail.com>
2019-03-26 13:53:53 -0500

    added exact={true} to one of the navlinks

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ const Header = () => (
         <h1>JM Designs</h1>
         <nav>
             <NavLink to="/" activeClassName="is-active" exact={true}>Home</NavLink>
-            <NavLink to="/portfolio" activeClassName="is-active">Portfolio</NavLink>
+            <NavLink to="/portfolio" activeClassName="is-active" exact={true}>Portfolio</NavLink>
             <NavLink to="/contact" activeClassName="is-active">Contact</NavLink>
         </nav>
     </header>
